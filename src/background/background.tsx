39331c37c61296e5c552f99fa536c2f110a5405e
Jonah Scheinerman <jscheinerman@palantir.com>
2017-04-10 20:59:50 +0100

    Simplify window creation

--- src/background/background.tsx
+++ src/background/background.tsx
@@ -12,38 +12,36 @@ export let currWindow: WindowInfo | null = null;
 const WIDTH = 600;
 const HEIGHT = 500;
 
-function createWindowHandler(command: Command) {
-    return () => {
-        let left = (window.screen.availWidth / 2) - (WIDTH / 2);
-        let options: chrome.windows.CreateData = {
-            url: `site/index.html#/${command}`,
-            focused: true,
-            width: WIDTH,
-            height: HEIGHT,
-            left,
-            top: 0,
-            type: "popup" 
-        };
-        chrome.windows.create(options, window => {
-            if (window) {
-                currWindow = {
-                    command,
-                    id: window.id
-                };
-            }
-        });
+function createWindow(command: Command) {
+    let left = (window.screen.availWidth / 2) - (WIDTH / 2);
+    let options: chrome.windows.CreateData = {
+        url: `site/index.html#/${command}`,
+        focused: true,
+        width: WIDTH,
+        height: HEIGHT,
+        left,
+        top: 0,
+        type: "popup" 
     };
+    chrome.windows.create(options, window => {
+        if (window) {
+            currWindow = {
+                command,
+                id: window.id
+            };
+        }
+    });
 }
 
 function onCommand(command: Command) {
     if (currWindow) {
         if (currWindow.command !== command) {
-            chrome.windows.remove(currWindow.id, createWindowHandler(command));
+            chrome.windows.remove(currWindow.id, () => createWindow(command));
         } else {
             chrome.windows.remove(currWindow.id);
         }
     } else {
-        createWindowHandler(command)();
+        createWindow(command);
     }
 }
 
